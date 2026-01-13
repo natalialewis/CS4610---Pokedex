259937cf242559eb2121ba8359a1dc30ff893658
Natalia Lewis <nattylewis11@gmail.com>
2026-01-12 21:22:31 -0700

    Added api fetchs for the lists of all generations, moves, and locations

--- utils/pokemonapi.ts
+++ utils/pokemonapi.ts
@@ -1,10 +1,10 @@
 // Type for list of all Pokemon
 export type PokemonList = {
-  count: number; // total number of Pokemon
-  results: { // array of Pokemon entries
-    name: string;
-    url: string;
-  }[];
+    count: number; // total number of Pokemon
+    results: { // array of Pokemon entries
+        name: string;
+        url: string;
+    }[];
 };
 
 // Function to fetch list of all Pokemon
@@ -12,4 +12,52 @@ export async function fetchPokemonList() {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10000');
     const pokemon = await response.json() as PokemonList;
     return pokemon;
+}
+
+// Type for list all locations
+export type LocationList = {
+    count: number; // total number of locations
+    results: { // array of location entries
+        name: string;
+        url: string;
+    }[];
+};
+
+// Function to fetch list of all locations
+export async function fetchLocationList() {
+    const response = await fetch('https://pokeapi.co/api/v2/location?limit=10000');
+    const locations = await response.json() as LocationList;
+    return locations;
+}
+
+// Type for list all moves
+export type MoveList = {
+    count: number; // total number of moves
+    results: { // array of move entries
+        name: string;
+        url: string;
+    }[];
+};
+
+// Function to fetch list of all moves
+export async function fetchMoveList() {
+    const response = await fetch('https://pokeapi.co/api/v2/move?limit=10000');
+    const moves = await response.json() as MoveList;
+    return moves;
+}
+
+// Type for list all generations
+export type GenerationList = {
+    count: number; // total number of generations
+    results: { // array of generation entries
+        name: string;
+        url: string;
+    }[];
+};
+
+// Function to fetch list of all generations
+export async function fetchGenerationList() {
+    const response = await fetch('https://pokeapi.co/api/v2/generation?limit=10000');
+    const generations = await response.json() as GenerationList;
+    return generations;
 }
