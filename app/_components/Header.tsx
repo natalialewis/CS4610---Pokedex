35610948ffea35b8bb5530bcf6721e6a963dacf9
Natalia Lewis <nattylewis11@gmail.com>
2026-01-13 13:04:31 -0700

    Added mobile sizes

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
-    <header className="top-0 h-45 w-full bg-[url('/images/pokemon-background-tile.webp')] bg-repeat bg-top-left flex items-center justify-center" style={{ backgroundSize: '150px 100px' }}>
+    <header className="top-0 h-35 lg:h-45 w-full bg-[url('/images/pokemon-background-tile.webp')] bg-repeat bg-top-left flex items-center justify-center" style={{ backgroundSize: '150px 100px' }}>
       {/* Pokédex Title and Subtitle */}
-      <div className="flex flex-col gap-6 justify-center items-center py-3 px-3 bg-[var(--pokemon-red)]/92 rounded-lg shadow-md">
-        <h1 className="font-[Pokemon] text-7xl">Pokédex</h1>
-        <p className="text-xl">
+      <div className="flex flex-col gap-3 lg:gap-6 justify-center items-center py-3 px-3 bg-[var(--pokemon-red)]/92 rounded-lg shadow-md">
+        <h1 className="font-[Pokemon] lg:text-7xl text-3xl">Pokédex</h1>
+        <p className="text-sm lg:text-xl">
           The Ultimate Pokémon Encyclopedia
         </p>
       </div>
